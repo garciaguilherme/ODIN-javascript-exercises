2658c49efae0dd6498388be63e4b157ca5e99d2d
Guilherme da Costa Garcia <44306931+garciaguilherme@users.noreply.github.com>
2025-11-14 08:50:40 -0300

    Reverse string solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+        processedText = input.split("").reverse().join("");
+        //return processedText;
+        return processedText;
 };
 
+console.log(reverseString("Mañana mañana"));
 // Do not edit below this line
 module.exports = reverseString;

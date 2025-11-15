0d3ac4c08ce3d7fafbfb5ab7b0017fe8185b8008
Guilherme <gakimuro@gmail.com>
2025-11-15 08:43:02 -0300

    Repeat string solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(toReverse) {
+    
+    let a = [];
+    a.push(toReverse;)
+    //let stringArray = toReverse.lenght;
+    // for(i=0; i <= stringArray ; i++){
+    //     return i;
+    // }
+    return a[0].lenght;
 };
-
+ console.log(reverseString("Caco"));
 // Do not edit below this line
 module.exports = reverseString;
